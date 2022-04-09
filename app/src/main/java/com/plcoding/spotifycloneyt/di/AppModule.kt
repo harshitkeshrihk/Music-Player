b9ae3b3fe4b34596d52b41b48cd2f50af87cc5ce
harshitkeshrihk <harshitkeshrihk@gmail.com>
2022-04-09 22:26:10 +0530

    First

---
+++ app/src/main/java/com/plcoding/spotifycloneyt/di/AppModule.kt
@@ -0,0 +1,45 @@
+package com.plcoding.spotifycloneyt.di
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.plcoding.spotifycloneyt.R
+import com.plcoding.spotifycloneyt.adapters.SwipeSongAdapter
+import com.plcoding.spotifycloneyt.exoplayer.MusicServiceConnection
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    //provide dependency which we will need for the lifetime of application
+    //glide instance and we will set default options for once here and will use many times
+
+    @Singleton
+    @Provides
+    fun provideMusicServiceConnection(
+        @ApplicationContext context: Context
+    ) = MusicServiceConnection(context)
+
+    @Singleton
+    @Provides
+    fun provideSongAdapter() = SwipeSongAdapter()
+
+    @Singleton    //ensures that only one instance should be created
+    @Provides
+    fun provideGlideInstance(
+        @ApplicationContext context: Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions()
+            .placeholder(R.drawable.ic_image)
+            .error(R.drawable.ic_image)
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+    )
+
+}
